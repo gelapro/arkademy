004cab98a9df8ca755dee451644aba7ef69bd3bb
gelapro <46478819+gelapro@users.noreply.github.com>
2019-01-15 15:59:19 +0700

    Add files via upload

---
+++ src2/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Coffee from './components/Coffee';
+import Glass from './components/Glass';
+import Saucer from './components/Saucer';
+
+export default class App extends Component{
+
+   
+
+    render(){
+      console.log('render');
+      return (
+        <div>
+          <Coffee 
+          waterVolume={1} 
+          bean="robusta" 
+          type="expresso"
+          />
+          <Glass color="red"/>
+          <Saucer width={100}/>
+        </div>
+      )
+    }
+
+}
