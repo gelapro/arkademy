1dfc77c99024eb3d8b912cdba46ee5c3bf0709c3
gelapro <46478819+gelapro@users.noreply.github.com>
2019-01-16 14:59:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const express = require("express");
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = expess();
 
-app.get('siswa/',(request,response)=>{
-    response.end("menampilkan siswa");
-})
-
-app.get('/siswa/:nama', (request,response)=>{
-    // Proses dengan siswa bernama x
-    let namaSiswa = request.params.nama;
-    response.end("Menampilkan siswa dengan nama : " + namaSiswa);
-})
-
-app.post('/siswa', (request,response=>{
-    let namaSiswa = request.body.nama;
-    let alamatSiswa = request.body.alamat;
-
-    response.end("Siswa baru! nama : " + namaSiswa + ", alamat : "+ alamatSiswa);
-}))
+app.listen(process.env.port || 5000, function(){
+    console.log('express app now listening request');
+});
